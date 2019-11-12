f1b1f548b58f3a1aaede391ba8e97706d614f550
John Doe <30752740@qq.com>
2019-11-12 11:51:30 +0800

    更新

--- shell/cluster/kafka.sh
+++ shell/cluster/kafka.sh
@@ -1,18 +1,17 @@
 #!/bin/bash
-
+kafka_home=/opt/module/kafka_2.11-0.11.0.2
 case $1 in
     start)
         for host in hadoop102 hadoop103 hadoop104 ; do
             echo "========== $host ========="
-        ssh $host "source /etc/profile; nohup /opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-start.sh /opt/module/kafka_2.11-0.11.0.2/config/server.properties >/opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-start.sh /opt/module/kafka_2.11-0.11.0.2/config/server.properties/kafka.log 2>&1
-   & "
+        ssh $host "source /etc/profile; nohup $kafka_home/bin/kafka-server-start.sh $kafka_home/config/server.properties >$kafka_home/kafka.log 2>&1 & "
         done
 
        ;;
     stop)
             for host in hadoop102 hadoop103 hadoop104 ; do
                 echo "========== $host ========="
-                ssh $host "source /etc/profile; nohup /opt/module/kafka_2.11-0.11.0.2/bin/kafka-server-stop.sh "
+                ssh $host "source /etc/profile; nohup $kafka_home/bin/kafka-server-stop.sh "
             done
 
            ;;
